fee8e222eaf2eef650dba1e73b8b81f625c2f0ea
ShyWebDev <c1216sg@gmail.com>
2022-04-14 02:24:14 +0900

    test05

--- src/main/java/com/example/discoveryservice/DiscoveryserviceApplication.java
+++ src/main/java/com/example/discoveryservice/DiscoveryserviceApplication.java
@@ -8,6 +8,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @EnableEurekaServer
 public class DiscoveryserviceApplication {
     //0-1-0
+    //0-5-0
     public static void main(String[] args) {
         SpringApplication.run(DiscoveryserviceApplication.class, args);
     }
