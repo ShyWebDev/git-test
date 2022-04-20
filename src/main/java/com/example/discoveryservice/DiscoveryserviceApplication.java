188edd7c4f11ac0d7432a2e566c108026063e1fa
ShyWebDev <c1216sg@gmail.com>
2022-04-20 14:18:44 +0900

    fc

--- src/main/java/com/example/discoveryservice/DiscoveryserviceApplication.java
+++ src/main/java/com/example/discoveryservice/DiscoveryserviceApplication.java
@@ -7,7 +7,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @SpringBootApplication
 @EnableEurekaServer
 public class DiscoveryserviceApplication {
-    
+    //000000
     //0-1-0
     //0-5-0
     //0-6-0_1
